28f764d098e13f5b094a3e17378ad6f253e73515
BuildTools <unconfigured@null.spigotmc.org>
2021-12-24 19:56:13 +0100

    changes

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"strings"
 )
 
+type CommandExecutor interface {
+}
+
+type ConsoleCommand struct {
+	command string
+	args    []string
+	hasArgs bool
+}
+
 func readCommand(reader *bufio.Reader) []string {
 	text, _ := reader.ReadString('\n')
 	text = strings.ReplaceAll(text, "\r", "")
@@ -23,14 +32,25 @@ func readCommand(reader *bufio.Reader) []string {
 	return finalCommand
 }
 
+func (cmd *ConsoleCommand) Parse(args []string) {
+	cmd.command = args[0]
+	if len(args) > 1 {
+		cmd.args = args[1:]
+	} else {
+		cmd.args = nil
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	running := true
+	var cmd ConsoleCommand
 	for running {
 		args := readCommand(reader)
 		if len(args) > 0 {
-			if len(args) == 1 {
-				if args[0] == "!" {
+			cmd.Parse(args)
+			if cmd.command == "!" {
+				if cmd.args == nil {
 					running = false
 				}
 			}
